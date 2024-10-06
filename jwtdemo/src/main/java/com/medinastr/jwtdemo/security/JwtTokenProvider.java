4855555b6fdcbb9c81f8db739cbfc2143ce0be80
medinastr <pedromstracieri@gmail.com>
2024-10-06 09:59:55 -0300

    adding PostConstructor to Provider

--- jwtdemo/src/main/java/com/medinastr/jwtdemo/security/JwtTokenProvider.java
+++ jwtdemo/src/main/java/com/medinastr/jwtdemo/security/JwtTokenProvider.java
@@ -1,9 +1,17 @@
 package com.medinastr.jwtdemo.security;
 
+import com.auth0.jwt.algorithms.Algorithm;
+import com.medinastr.jwtdemo.model.dto.security.TokenDTO;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class JwtTokenProvider {
@@ -19,5 +27,14 @@ public class JwtTokenProvider {
     @Autowired
     public JwtTokenProvider(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    Algorithm algorithm = null;
+
+    @PostConstruct
+    protected void init() {
+        // pega o que está na secret e encripta ela e seta novamente o valor
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        algorithm = Algorithm.HMAC256(secretKey.getBytes());
     }
 }
