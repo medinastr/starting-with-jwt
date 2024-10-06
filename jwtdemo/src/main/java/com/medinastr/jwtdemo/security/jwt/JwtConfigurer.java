a0193726066e1fea058cf87fd2fa7f8eba5dada1
medinastr <pedromstracieri@gmail.com>
2024-10-06 16:15:40 -0300

    method configure implemented

--- jwtdemo/src/main/java/com/medinastr/jwtdemo/security/jwt/JwtConfigurer.java
+++ jwtdemo/src/main/java/com/medinastr/jwtdemo/security/jwt/JwtConfigurer.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
@@ -17,6 +18,7 @@ public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilt
 
     @Override
     public void configure(HttpSecurity builder) throws Exception {
-        super.configure(builder);
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        builder.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
